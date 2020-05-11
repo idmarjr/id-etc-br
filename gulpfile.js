idmarjr/id-etc-br

commit e2fac9ee943fcb5a484b2b31ee700c1ea68f1dee

Use consistent scss name acroess the border
- Rename sass/ to scss/ folder;
- ename sassInput to scssInput constant in gulpfile.js;

gulpfile.js
@@ -11,7 +11,7 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 // Location variables
-const sassInput = './sass/**/*.scss';
+const scssInput = './sass/**/*.scss';
 const jsInput = './js/**/*.js'
 const sassOutput = './dist/css/';
 const distJS = './dist/js'
@@ -24,7 +24,7 @@ let sassOptions = {
 
 function scssCompile() {
 	return gulp
-	.src(sassInput)
+	.src(scssInput)
 	.pipe(sourcemaps.init())
 	.pipe(sass(sassOptions).on('error', sass.logError))
 	.pipe(postcss( [ autoprefixer(), cssnano() ] ))
@@ -43,7 +43,7 @@ function jsCompile() {
 function watch() {
 	return gulp
 	.watch(
-		[sassInput, jsInput],
+		[scssInput, jsInput],
 		parallel(scssCompile, jsCompile)
 	)
 	.on('change', function(path) {

commit 20b726ed6d0e36a5d0efc65baadc8ee072cf85cd

Fix errors caused by commit e2fac9e
Few paths and variables were not properly changed in the commit mentioned above.

gulpfile.js
@@ -11,9 +11,9 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 // Location variables
-const scssInput = './sass/**/*.scss';
+const scssInput = './scss/**/*.scss';
 const jsInput = './js/**/*.js'
-const sassOutput = './dist/css/';
+const scssOutput = './dist/css/';
 const distJS = './dist/js'
 
 // Sass output options
@@ -29,7 +29,7 @@ function scssCompile() {
 	.pipe(sass(sassOptions).on('error', sass.logError))
 	.pipe(postcss( [ autoprefixer(), cssnano() ] ))
 	.pipe(sourcemaps.write())
-	.pipe(gulp.dest(sassOutput))
+	.pipe(gulp.dest(scssOutput))
 };
 
 function jsCompile() {