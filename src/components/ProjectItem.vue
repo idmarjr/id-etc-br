d9da479de61f3d737f61c4d9862a6480f7265fd4
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-06 17:24:51 +0100

    Add ProjectItem.vue markup
                - Skeleton HTML
                - No styles yet
                - No Logic yet

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<li>
+		<header class="projectHeader">
+			<h3>Company Name</h3>
+			<p>Project Name</p>
+		</header>
+		<div class="projectDescription">
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, omnis. Doloribus expedita accusamus, quas sunt fugit.</p>
+		</div>
+		<div class="projectScreenshot">
+			<img src="" alt="Project image description" />
+		</div>
+	</li>
+</template>
+
+<style scoped>
+
+</style>
