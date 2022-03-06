4992ac14e8dc8a0564a00ce345395d0fa70bd5ea
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-06 17:28:48 +0100

    Add SocialItem.vue markup
    - Skeleton HTML
    - No styles yet
    - No Logic yet

---
+++ src/components/SocialItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<li>
+		<a href="" target="_blank" rel="noopener noreferrer">Social Media Name</a>
+	</li>
+</template>
+
+<style scoped>
+
+</style>
