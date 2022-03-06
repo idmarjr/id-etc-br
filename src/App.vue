ce0a6af70f5fe785b7f901c83f96251132446f2a
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-06 15:40:57 +0100

    Add MainHeader.vue markup
    - Skeleton HTML
    - Import it to App.vue
    - No styles yet

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+import MainHeader from './components/MainHeader.vue'
 </script>
 
 <template>
-  <MaingHeader />
-  <About />
-  <Work />
-  <Social />
+  <MainHeader />
 </template>
 
 <style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<header class="mainHeader">
+		<div class="wrapper">
+			<h1 class="vhide">Idmar Ramos Juniuor - Product designer and frontend developer</h1>
+			<img src="../assets/id-tulip-fields.png" alt="Idmar picture" />
+			<nav class="mainNav">
+				<ul>
+					<li><a href="#about">About</a></li>
+					<li><a href="#work">Work</a></li>
+					<li><a href="#social">Social</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</template>
+
+<style scoped>
+
+</style>
