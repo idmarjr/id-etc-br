d7c983785f7513d58e9ec80da0198781fdcf9a85
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-12 21:18:11 +0100

    SocialItem.vue styles

--- src/components/social/SocialItem.vue
+++ src/components/social/SocialItem.vue
@@ -3,11 +3,30 @@
 </script>
 
 <template>
-	<li>
+	<li class="socialItem">
 		<a href="" target="_blank" rel="noopener noreferrer">Social Media Name</a>
 	</li>
 </template>
 
 <style scoped>
+
+	.socialItem {
+		list-style: none;
+	}
+
+	.socialItem a {
+		display: block;
+		padding: 18px 12px;
+		border: 1px solid var(--primary-color);
+		margin-bottom: 12px;
+		font-size: 1.6em;
+		font-weight: 200;
+		text-align: center;
+		color: var(--primary-color);
+	}
+
+	.socialItem a:hover {
+		text-decoration: underline;
+	}
 
 </style>
