10970dfe8a2ee8f02b2028653e3daa12c3f582b5
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-12 20:22:48 +0100

    About.vue styles

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,14 +3,35 @@
 </script>
 
 <template>
-	<div id="about">
+	<section id="about">
 		<div class="wrapper">
-			<p class="higlight">Hello!</p>
-			<p>I'm <strong>Idmar</strong> a product desinger and front-end developer based in Amsterdam.</p>
+			<header class="intro">
+				<p class="higlight">Hello!</p>
+				<p>I'm <strong>Idmar</strong> a product desinger and front-end developer based in Amsterdam.</p>
+			</header>				
 		</div>
-	</div>
+	</section>
 </template>
 
 <style scoped>
+
+.intro {
+	max-width: 640px;
+	padding-top: 48px;
+	padding-bottom: 64px;
+	letter-spacing: 0.03em;
+}
+
+.intro .higlight {
+	font-size: 3.2em;
+	margin-bottom: 12px;
+	font-weight: 300;
+}
+
+.intro p {
+	font-size: 2.6em;
+	line-height: 1.4em;
+	font-weight: 100;
+}
 
 </style>
