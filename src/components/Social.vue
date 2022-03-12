f987f98f473222327687db26158d9d082395d944
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-12 23:53:52 +0100

    Update Social.vue
    - Add missing <section> with ID
    - Define margin bottom to better space between sections

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
-import SectionHeader from './sectionHeader.vue';
-import SocialItem from './social/SocialItem.vue';
-
+	import SectionHeader from './sectionHeader.vue';
+	import SocialItem from './social/SocialItem.vue';
 </script>
 
 <template>
-	<div class="wrapper">
-		<SectionHeader />
-		<ul>
-			<SocialItem />
-			<SocialItem />
-			<SocialItem />
-			<SocialItem />
-		</ul>
-	</div>
+	<section id="social">
+		<div class="wrapper">
+			<SectionHeader />
+			<ul>
+				<SocialItem />
+				<SocialItem />
+				<SocialItem />
+				<SocialItem />
+			</ul>
+		</div>
+	</section>
 </template>
 
 <style scoped>
+
+#social {
+	margin-bottom: 64px;
+}
 
 </style>
