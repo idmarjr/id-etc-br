cc20f862df2dcbe91ed6cc51e203e6129b9f3e6e
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-13 17:35:46 +0100

    convert intentation from tabs to spaces

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+	// This starter template is using Vue 3 <script setup> SFCs
+	// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
-import MainHeader from './components/MainHeader.vue'
-import About from './components/About.vue'
-import Work from './components/Work.vue';
-import Social from './components/Social.vue';
+	import MainHeader from './components/MainHeader.vue'
+	import About from './components/About.vue'
+	import Work from './components/Work.vue';
+	import Social from './components/Social.vue';
 </script>
 
 <template>
