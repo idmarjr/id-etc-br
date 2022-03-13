713c3bcafba8f5e1244032414273f282b152726c
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-13 17:31:30 +0100

    Update ESLinter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
 		// Override/add rules settings here:
 		'vue/html-indent': [ 'error', 'tab'],
 		'vue/singleline-html-element-content-newline': 'off',
+		'vue/multi-word-component-names': 'off',
 		'vue/html-self-closing': ['error', {
 			'html': {
 				'void': 'always'
