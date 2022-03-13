c63dab812a448fd3060cf9de04244ff39e344ae5
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-03-13 22:34:48 +0100

    Load data to Social.vue
    - Define data using ref()
    - Loop into data using v-for

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,6 +1,30 @@
 <script setup lang="ts">
+	import { ref } from 'vue';
 	import SectionHeader from './sectionHeader.vue';
 	import SocialItem from './social/SocialItem.vue';
+
+	const SocialItems = ref([
+		{
+			id: 1,
+			title: 'Github',
+			url: 'https://github.com/idmarjr',
+		},
+		{
+			id: 2,
+			title: 'Linkedin',
+			url: 'https://linkedin.com/idmarjr',
+		},
+		{
+			id: 3,
+			title: 'Twitter',
+			url: 'https://twitter.com/idmarjr',
+		},
+		{
+			id: 4,
+			title: 'Medium',
+			url: 'https://medium.com/@idmarjr',
+		}
+	])
 </script>
 
 <template>
@@ -8,10 +32,12 @@
 		<div class="wrapper">
 			<SectionHeader title="Social" />
 			<ul class="socialList">
-				<SocialItem />
-				<SocialItem />
-				<SocialItem />
-				<SocialItem />
+				<SocialItem
+					v-for="item in SocialItems"
+					:key="item.id"
+					:title="item.title"
+					:url="item.url"
+				/>
 			</ul>
 		</div>
 	</section>
