18366aebe5f9430f45cc5dae974f6daf2d41378f
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2025-10-06 16:33:34 -0300

    Use constant for Google Analytics ID in script files

--- googleAnalyticsScript-example.js
+++ googleAnalyticsScript-example.js
@@ -1,11 +1,11 @@
-// Google Analytics configuration
-// Replace "YOUR-GA-ID" at lines 5 and 11 with your Google Analytics Id
+const GA_ID = 'YOUR-GA-ID';
+
 const script = document.createElement('script');
 script.async = true;
-script.src = 'https://www.googletagmanager.com/gtag/js?id=YOUR-GA-ID';
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
 document.head.appendChild(script);
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'YOUR-GA-ID');
+gtag('config', GA_ID);
